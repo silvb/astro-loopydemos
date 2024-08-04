bd31ee5725bc175c3953aeac930ed012d5d98a5b
silvio-wtfoxtrot <silvio@wtfoxtrot.de>
2024-08-04 17:59:35 +0200

    feat: responsive post preview layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      screens: {
+        xs: "375px",
+      },
       fontFamily: {
         sans: ["Mulish Variable", ...defaultTheme.fontFamily.sans],
         mono: ["VT323", ...defaultTheme.fontFamily.mono],
