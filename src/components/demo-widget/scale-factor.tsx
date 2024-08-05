6b8306f608289198f63691b956c01b0263614758
silvio-wtfoxtrot <silvio@wtfoxtrot.de>
2024-08-05 23:16:36 +0200

    feat: optimize mobile pedal scaling

--- src/components/demo-widget/scale-factor.tsx
+++ src/components/demo-widget/scale-factor.tsx
@@ -1,15 +1,28 @@
 import {
   createEffect,
   createSignal,
+  onCleanup,
   onMount,
   type ParentComponent,
 } from "solid-js"
 import { demoState } from "./demo-state-store"
+// import breakpoint from tailwind config
+import resolveConfig from "tailwindcss/resolveConfig"
+import tailwindConfig from "tailwind.config.mjs"
 
 export const ScaleFactor: ParentComponent = (props) => {
   const { activePedals, widthTab } = demoState
   const [containerWidth, setContainerWidth] = createSignal(window.innerWidth)
   const [scale, setScale] = createSignal(1)
+  const [isMobile, setIsMobile] = createSignal(true)
+  const breakpoint = resolveConfig(tailwindConfig).theme.screens.sm
+
+  const handleResize = () => {
+    setContainerWidth(
+      document.getElementById("scale-container")?.offsetWidth ?? 0
+    )
+    setIsMobile(window.matchMedia(`(max-width: ${breakpoint})`).matches)
+  }
 
   createEffect(() => {
     const currentCompoundWidth = activePedals().reduce(
@@ -17,15 +30,20 @@ export const ScaleFactor: ParentComponent = (props) => {
       0
     )
 
-    setScale(Math.min(1, containerWidth() / currentCompoundWidth))
+    const mobileFactor = isMobile() ? 0.75 : 1
+
+    setScale(
+      Math.min(1, containerWidth() / (currentCompoundWidth * mobileFactor))
+    )
   })
 
   onMount(() => {
-    window.addEventListener("resize", () => {
-      setContainerWidth(
-        document.getElementById("scale-container")?.offsetWidth ?? 0
-      )
-    })
+    handleResize()
+    window.addEventListener("resize", handleResize)
+  })
+
+  onCleanup(() => {
+    window.removeEventListener("resize", handleResize)
   })
 
   return (
