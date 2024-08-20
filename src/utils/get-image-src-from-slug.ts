532628fd833adbf667495a2be98e2a54a1b71d98
silvio-wtfoxtrot <silvio@wtfoxtrot.de>
2024-08-20 09:03:48 +0200

    feat: add structured data

--- src/utils/get-image-src-from-slug.ts
+++ src/utils/get-image-src-from-slug.ts
@@ -1,4 +1,19 @@
-export const getImageSrcFromSlug = (slug: string) => {
+export const getImageSrcFromSlug = (slug: string, isSeo?: boolean) => {
+  if (isSeo) {
+    const images = import.meta.glob<{ default: ImageMetadata }>(
+      "/src/images/seo-preview/*.{jpeg,jpg}"
+    )
+
+    const imageSrc =
+      images[`/src/images/seo-preview/${slug}.jpeg`] ||
+      images[`/src/images/seo-preview/${slug}.jpg`]
+
+    if (!imageSrc)
+      throw new Error(`No image exists for "${slug}" in src/images/seo-preview`)
+
+    return imageSrc()
+  }
+
   const images = import.meta.glob<{ default: ImageMetadata }>(
     "/src/images/*.{jpeg,jpg,png,webp}"
   )
