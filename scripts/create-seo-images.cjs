900c50dd69ce28265ed38957b306030104231a84
silvio-wtfoxtrot <silvio@wtfoxtrot.de>
2024-08-20 23:06:22 +0200

    feat: add crushturnal basic files

---
+++ scripts/create-seo-images.cjs
@@ -0,0 +1,72 @@
+const fs = require("fs")
+const path = require("path")
+const matter = require("gray-matter")
+const { execSync } = require("child_process")
+
+const imageSourcePath = "src/images"
+const targetDirectoryPath = "src/images/seo-preview"
+const imageExtensions = [".png", ".webp"]
+const fillColor = "#9580ff"
+
+const convertImage = (imagePath, outputPath) => {
+  const identifyOutput = execSync(
+    `identify -format "%w %h" ${imagePath}`
+  ).toString()
+  const imageName = imagePath.split("/").pop().split(".")[0]
+  const [width, height] = identifyOutput.split(" ").map(Number)
+  const longestSide = Math.max(width, height)
+  const extendLength = longestSide + 60
+
+  const convertCmd = `convert -size ${extendLength}x${extendLength} xc:${fillColor} "${imagePath}" -gravity center -composite -resize 400x400 "${outputPath}"`
+
+  execSync(convertCmd)
+
+  console.log(`Processed ${imageName}`)
+}
+
+fs.readdirSync(imageSourcePath)
+  .filter(file => imageExtensions.includes(path.extname(file).toLowerCase()))
+  .forEach(image => {
+    const imagePath = path.join(imageSourcePath, image)
+    const imageWithoutExtension = image.split(".")[0]
+    const outputPath = path.join(
+      targetDirectoryPath,
+      `${imageWithoutExtension}.jpeg`
+    )
+
+    try {
+      convertImage(imagePath, outputPath)
+    } catch (error) {
+      console.error(`Error processing ${image}: ${error}`)
+    }
+  })
+
+const postSourcePath = "src/content/posts"
+
+fs.readdirSync(postSourcePath).forEach(post => {
+  const mdxPath = path.join(postSourcePath, post, "index.mdx")
+  const mdxFile = fs.readFileSync(mdxPath, "utf8")
+  const parsedMdx = matter(mdxFile)
+
+  if (!parsedMdx.data.coverImage && parsedMdx.data.pedalImages.length > 0) {
+    const pedalsSeoImages = parsedMdx.data.pedalImages
+      .map(pedalImagePath => {
+        return pedalImagePath.split("/").pop().split(".")[0]
+      })
+      .map(pedalImageName => {
+        return path.join(targetDirectoryPath, `${pedalImageName}.jpeg`)
+      })
+
+    const gridSize = Math.ceil(Math.sqrt(pedalsSeoImages.length))
+    const outputPathRaw = path.join(targetDirectoryPath, `${post}_raw.png`)
+    const outputPath = path.join(targetDirectoryPath, `${post}.jpeg`)
+
+    const montageCmd = `montage ${pedalsSeoImages.join(
+      " "
+    )} -tile ${gridSize}x${gridSize} -geometry +0+0 -background '${fillColor}' "${outputPathRaw}"`
+
+    execSync(montageCmd)
+    convertImage(outputPathRaw, outputPath)
+    execSync(`rm ${outputPathRaw}`)
+  }
+})
---
+++ src/content/demos/collision-devices-crushturnal.md
@@ -0,0 +1,25 @@
+---
+builder: Collision Devices
+model: Crushturnal
+slug: collision-devices-crushturnal
+type: demo
+isSponsored: true
+date: 2024-08-20
+tags:
+  - bit crush
+  - experimental
+  - delay
+  - lo-fi
+externalLinks:
+  perfectCircuit: collision-devices-crushturnal
+  builderLink: ...
+---
+
+Write some copy about the Crushturnal by Collision Devices.
+
+### Signal Chain
+
+- Fidelity Guitars Stellarosa Lite II with Lollar Firebird pickups (both pickups)
+- Crushturnal
+- [Mixwave Benson Chimera Plugin](https://www.mixwave.net/products/benson-chimera)
+- A bit of room-like reverb from the [Valhalla Vintage Verb plugin](https://valhalladsp.com/shop/reverb/valhalla-vintage-verb/)
