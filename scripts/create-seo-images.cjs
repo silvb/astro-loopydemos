a0ffdf00a5a36453f6664fb65d9493e8e52a6154
silvio-wtfoxtrot <silvio@wtfoxtrot.de>
2024-09-17 17:29:24 +0200

    chore: add a bunch of images and placeholder files

--- scripts/create-seo-images.cjs
+++ scripts/create-seo-images.cjs
@@ -7,67 +7,87 @@ const imageSourcePath = "src/images"
 const targetDirectoryPath = "src/images/seo-preview"
 const imageExtensions = [".png", ".webp"]
 const fillColor = "#9580ff"
+const postSourcePath = "src/content/posts"
+const presetSource = "src/content/presets"
 
-const convertImage = (imagePath, outputPath) => {
-  const identifyOutput = execSync(
-    `identify -format "%w %h" ${imagePath}`
-  ).toString()
-  const imageName = imagePath.split("/").pop().split(".")[0]
-  const [width, height] = identifyOutput.split(" ").map(Number)
-  const longestSide = Math.max(width, height)
-  const extendLength = longestSide + 60
+// const convertImage = (imagePath, outputPath) => {
+//   const identifyOutput = execSync(
+//     `identify -format "%w %h" ${imagePath}`
+//   ).toString()
+//   const imageName = imagePath.split("/").pop().split(".")[0]
+//   const [width, height] = identifyOutput.split(" ").map(Number)
+//   const longestSide = Math.max(width, height)
+//   const extendLength = longestSide + 60
 
-  const convertCmd = `convert -size ${extendLength}x${extendLength} xc:${fillColor} "${imagePath}" -gravity center -composite -resize 400x400 "${outputPath}"`
+//   const convertCmd = `convert -size ${extendLength}x${extendLength} xc:${fillColor} "${imagePath}" -gravity center -composite -resize 400x400 "${outputPath}"`
 
-  execSync(convertCmd)
+//   execSync(convertCmd)
 
-  console.log(`Processed ${imageName}`)
-}
+//   console.log(`Processed ${imageName}`)
+// }
 
-fs.readdirSync(imageSourcePath)
-  .filter(file => imageExtensions.includes(path.extname(file).toLowerCase()))
-  .forEach(image => {
-    const imagePath = path.join(imageSourcePath, image)
-    const imageWithoutExtension = image.split(".")[0]
-    const outputPath = path.join(
-      targetDirectoryPath,
-      `${imageWithoutExtension}.jpeg`
-    )
+// fs.readdirSync(imageSourcePath)
+//   .filter(file => imageExtensions.includes(path.extname(file).toLowerCase()))
+//   .forEach(image => {
+//     const imagePath = path.join(imageSourcePath, image)
+//     const imageWithoutExtension = image.split(".")[0]
+//     const outputPath = path.join(
+//       targetDirectoryPath,
+//       `${imageWithoutExtension}.jpeg`
+//     )
 
-    try {
-      convertImage(imagePath, outputPath)
-    } catch (error) {
-      console.error(`Error processing ${image}: ${error}`)
-    }
-  })
+//     try {
+//       convertImage(imagePath, outputPath)
+//     } catch (error) {
+//       console.error(`Error processing ${image}: ${error}`)
+//     }
+//   })
 
 // TODO: refactor this and make it work with new file structure
-// const postSourcePath = "src/content/posts"
 
-// fs.readdirSync(postSourcePath).forEach(post => {
-//   const mdxPath = path.join(postSourcePath, post, "index.mdx")
-//   const mdxFile = fs.readFileSync(mdxPath, "utf8")
-//   const parsedMdx = matter(mdxFile)
+fs.readdirSync(postSourcePath).forEach(post => {
+  const slug = post.split(".md")[0]
+  console.log(slug)
 
-//   if (!parsedMdx.data.coverImage && parsedMdx.data.pedalImages.length > 0) {
-//     const pedalsSeoImages = parsedMdx.data.pedalImages
-//       .map(pedalImagePath => {
-//         return pedalImagePath.split("/").pop().split(".")[0]
-//       })
-//       .map(pedalImageName => {
-//         return path.join(targetDirectoryPath, `${pedalImageName}.jpeg`)
-//       })
+  const possibleImagePath = path.join(imageSourcePath, slug + ".png")
 
-//     const gridSize = Math.ceil(Math.sqrt(pedalsSeoImages.length))
-//     const outputPathRaw = path.join(targetDirectoryPath, `${post}_raw.png`)
-//     const outputPath = path.join(targetDirectoryPath, `${post}.jpeg`)
+  if (fs.existsSync(possibleImagePath)) return
 
-//     const montageCmd = `montage ${pedalsSeoImages.join(
-//       " "
-//     )} -tile ${gridSize}x${gridSize} -geometry +0+0 -background '${fillColor}' "${outputPathRaw}"`
+  const presetPath = path.join(presetSource, slug + ".presets.json")
+  const presetData = JSON.parse(fs.readFileSync(presetPath))
 
-//     execSync(montageCmd)
-//     convertImage(outputPathRaw, outputPath)
-//     execSync(`rm ${outputPathRaw}`)
-//   }
-// })
+  const pedals =
+    presetData.presets[0]?.comparison?.map(comp => comp.pedalSlug) ||
+    presetData.presets[0]?.chain?.map(chainEl => chainEl.pedalSlug)
+
+  const pedalImagePaths = pedals.map(pedal => {
+    return path.join(imageSourcePath, pedal + ".png")
+  })
+
+  console.log({
+    pedalImagePaths,
+  })
+  const gridSize = Math.ceil(Math.sqrt(pedalImagePaths.length))
+  const outputPathRaw = path.join(targetDirectoryPath, `${post}_raw.png`)
+  const outputPath = path.join(targetDirectoryPath, `${post}.jpeg`)
+  const montageCmd = `montage ${pedalImagePaths.join(
+    " "
+  )} -tile ${gridSize}x${gridSize} -geometry +0+0 -background '${fillColor}' "${outputPathRaw}"`
+
+  execSync(montageCmd)
+  convertImage(outputPathRaw, outputPath)
+  execSync(`rm ${outputPathRaw}`)
+  // const mdxFile = fs.readFileSync(mdxPath, "utf8")
+  // const parsedMdx = matter(mdxFile)
+
+  // if (!parsedMdx.data.coverImage && parsedMdx.data.pedalImages.length > 0) {
+  //   const pedalsSeoImages = parsedMdx.data.pedalImages
+  //     .map(pedalImagePath => {
+  //       return pedalImagePath.split("/").pop().split(".")[0]
+  //     })
+  //     .map(pedalImageName => {
+  //       return path.join(targetDirectoryPath, `${pedalImageName}.jpeg`)
+  //     })
+
+  // }
+})
