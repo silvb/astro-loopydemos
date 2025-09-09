39ea988904da214eff0e889e00d1d193276ad3b9
silvb <silvio.schmidt@posteo.net>
2025-09-09 20:50:21 +0200

    feat: file setup

--- src/components/demo-widget/controls/knob/jhs-knob.tsx
+++ src/components/demo-widget/controls/knob/jhs-knob.tsx
@@ -28,13 +28,13 @@ export const JhsKnob: Component<JhsKnobProps> = props => {
           cy="60"
           r="56"
           fill={mergedProps.colors.primary}
-          stroke-width="4"
+          stroke-width="3"
           stroke={mergedProps.colors.secondary}
         />
         <path
           fill={mergedProps.colors.primary}
           stroke={mergedProps.colors.secondary}
-          stroke-width="4"
+          stroke-width="3"
           d="M104.37701,29.5816271 L115.312765,54.8801148 L131.754021,77 L115.312765,99.1198852 L104.37701,124.418373 L77,121.23977 L49.6229896,124.418373 L38.687235,99.1198852 L22.2459793,77 L38.687235,54.8801148 L49.6229896,29.5816271 L77,32.7602296 L104.37701,29.5816271 Z"
           transform="rotate(30 60 60) translate(-17 -16.073)"
         />
--- src/content/demos/lateral-phonics-marrakesh-console-mk2.md
+++ src/content/demos/lateral-phonics-marrakesh-console-mk2.md
@@ -4,7 +4,7 @@ model: Marrakesh Console Mk2
 slug: lateral-phonics-marrakesh-console-mk2
 type: demo
 isSponsored: true
-date: 2025-09-06
+date: 2025-09-10
 tags:
   - fuzzdrive
   - overdrive
@@ -22,5 +22,5 @@ Write some copy about the Marrakesh Console Mk2 by Lateral Phonics.
 
 - Fidelity Guitars Stellarosa with Mojo Lipstick Pickups (bridge pickup)
 - Marrakesh Console Mk2
-- [Mixwave Benson Chimera Plugin*](https://sweetwater.sjv.io/B0N2PL)
+- [Mixwave Benson Chimera Plugin\*](https://sweetwater.sjv.io/B0N2PL)
 - A bit of room-like reverb from the [Valhalla Vintage Verb plugin](https://valhalladsp.com/shop/reverb/valhalla-vintage-verb/)
