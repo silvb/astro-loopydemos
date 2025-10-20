f0624834d3047ed833b5fd21d1f6e73cd2efa13e
silvio-wtfoxtrot <silvio@wtfoxtrot.de>
2025-10-20 17:20:18 +0200

    feat: update audio media root url

--- src/components/demo-widget/audio-player/constants.ts
+++ src/components/demo-widget/audio-player/constants.ts
@@ -1,4 +1,5 @@
 export const CLEAN_TONE = "CLEAN_TONE"
 export const BACKING_TRACK = "BACKING_TRACK"
 export const MAX_DECODED_AUDIO_NUM = 7
-export const MEDIA_ROOT_URL = "https://loopydemos.us-east-1.linodeobjects.com"
+// export const MEDIA_ROOT_URL = "https://loopydemos.us-east-1.linodeobjects.com"
+export const MEDIA_ROOT_URL = "https://audio.loopydemos.com"
